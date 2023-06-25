347f2378450a3dfc72fe17dfde7fcca9f0bdeb34
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-25 09:50:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// setup express
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.status(200).send('hello from the server side')
+})
+
+
+
+
+//start the server 
+const port = 3000
+app.listen(port, ()=>{
+    console.log('listening on 3000')
+})
