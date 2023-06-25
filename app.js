44e7eb53e4a8ab48f1ed0f826d2694c65f61cd95
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-25 11:16:49 -0600

    Dynamicalled added new tour to local data

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
 // setup express
-const fs = require('fs')
+const fs = require("fs");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 //Express middleware
-app.use(express.json()) // parsing incoming json data, or undefined
+app.use(express.json()); // parsing incoming json data, or undefined
 
-//read local data 
-//  - blocking 
+//read local data
+//  - blocking
 //  - json.parse to convert JSON to objects
-const allTours = JSON.parse(fs.readFileSync('./data/tours-simple.json'))
+const allTours = JSON.parse(fs.readFileSync("./data/tours-simple.json"));
 
 //********* Get *************
-app.get('/api/v1/tours', (req, res)=>{
-
-    res.status(200).json({
-        status: 'success', 
-        results: allTours.length,
-        data: {
-            tours: allTours
-        }
-    })
-    
-})
-
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: allTours.length,
+    data: {
+      tours: allTours,
+    },
+  });
+});
 
 //********* Post *************
-app.post('/api/v1/tours', (req, res) => {
-    console.log(req.body)
-    res.send('Hello')
+app.post("/api/v1/tours", (req, res) => {
+  const newID = allTours[allTours.length - 1].id + 1; // last tour id+1
+  const newTour = Object.assign({ id: newID }, req.body); // create a new object with json
 
-})
+  allTours.push(newTour);
 
+  // can't use blocking method inside a callback
+  fs.writeFile("./data/tours-simple.json", JSON.stringify(allTours), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 
-//start the server 
-const port = 3000
-app.listen(port, ()=>{
-    console.log('listening on 3000')
-})
+  console.log(req.body);
+});
+
+//start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log("listening on 3000");
+});
