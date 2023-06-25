ff6a81a704eba7fc3a814644000b22befd992798
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-25 11:07:53 -0600

    Added post api, and used express json middleware

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 
+//Express middleware
+app.use(express.json()) // parsing incoming json data, or undefined
 
 //read local data 
 //  - blocking 
-//  -json.parse to convert JSON to objects
+//  - json.parse to convert JSON to objects
 const allTours = JSON.parse(fs.readFileSync('./data/tours-simple.json'))
 
+//********* Get *************
 app.get('/api/v1/tours', (req, res)=>{
 
     res.status(200).json({
@@ -22,6 +25,13 @@ app.get('/api/v1/tours', (req, res)=>{
     
 })
 
+
+//********* Post *************
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body)
+    res.send('Hello')
+
+})
 
 
 //start the server 
