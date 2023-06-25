5a24998d542f609f5c7694d8a48db85342926e26
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-25 10:55:11 -0600

    FInished GET API with local data

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // setup express
+const fs = require('fs')
+
 const express = require('express')
 const app = express()
 
+
+//read local data 
+//  - blocking 
+//  -json.parse to convert JSON to objects
+const allTours = JSON.parse(fs.readFileSync('./data/tours-simple.json'))
+
 app.get('/api/v1/tours', (req, res)=>{
+
+    res.status(200).json({
+        status: 'success', 
+        results: allTours.length,
+        data: {
+            tours: allTours
+        }
+    })
     
 })
 
