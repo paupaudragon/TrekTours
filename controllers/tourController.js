586c3128a555c5383d8998a8119a7649fb3c52fb
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-26 10:17:33 -0600

    param middleware and chaining middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,92 +1,89 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 //read local data
 //  - blocking
 //  - json.parse to convert JSON to objects
-const filePath = path.join(__dirname, '..', 'data', 'tours-simple.json');
+const filePath = path.join(__dirname, "..", "data", "tours-simple.json");
 const allTours = JSON.parse(fs.readFileSync(filePath));
 
+exports.checkID = (req, res, next) => {
+  const tour = allTours.find((el) => el.id === Number(req.params.id)); //also could use req.params.id *1 ti convert string to number
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "not found tour",
+    });
+  }
+  next();
+};
 
- exports.getAllTours = (req, res) => {
-    res.status(200).json({
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "fail",
+      message: "missing name or price",
+    });
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: allTours.length,
+    data: {
+      tours: allTours,
+    },
+  });
+};
+
+exports.getATour = (req, res) => {
+  const tour = allTours.find((el) => el.id === Number(req.params.id));
+  console.log(req.params);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours: tour,
+    },
+  });
+};
+
+exports.createATour = (req, res) => {
+  const newID = allTours[allTours.length - 1].id + 1; // last tour id+1
+  const newTour = Object.assign({ id: newID }, req.body); // create a new object with json
+
+  allTours.push(newTour);
+
+  // can't use blocking method inside a callback
+  fs.writeFile("./data/tours-simple.json", JSON.stringify(allTours), (err) => {
+    res.status(201).json({
       status: "success",
-      results: allTours.length,
       data: {
-        tours: allTours,
+        tour: newTour,
       },
     });
-  }
+  });
 
-  exports.getATour = (req, res) => {
+  console.log(req.body);
+};
 
-    console.log(req.params)
+exports.updateATour = (req, res) => {
+  const tour = allTours.find((el) => el.id === Number(req.params.id));
 
-    const tour = allTours.find((el)=>el.id === Number(req.params.id)) //also could use req.params.id *1 ti convert string to number
-    if(!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: "not found tour"
-        })
-    }
-    res.status(200).json({
-      status: "success",
-      data: {
-        tours: tour,
-      },
-    });
-  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Updated",
+    },
+  });
+};
 
-  exports.createATour = (req, res) => {
-    const newID = allTours[allTours.length - 1].id + 1; // last tour id+1
-    const newTour = Object.assign({ id: newID }, req.body); // create a new object with json
-  
-    allTours.push(newTour);
-  
-    // can't use blocking method inside a callback
-    fs.writeFile("./data/tours-simple.json", JSON.stringify(allTours), (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    });
-  
-    console.log(req.body);
-  }
+exports.deleteATour = (req, res) => {
+  const tour = allTours.find((el) => el.id === Number(req.params.id));
 
-  exports.updateATour = (req, res)=>{
-
-    const tour = allTours.find((el)=>el.id === Number(req.params.id)) //also could use req.params.id *1 ti convert string to number
-    if(!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: "not found tour"
-        })
-    }
-
-    res.status(200).json({
-        status: 'success', 
-        data: {
-            tour: 'Updated'
-        }
-    })
-}
-
- exports.deleteATour = (req, res)=>{
-
-    const tour = allTours.find((el)=>el.id === Number(req.params.id)) //also could use req.params.id *1 ti convert string to number
-    if(!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: "not found tour"
-        })
-    }
-
-    res.status(204).json({
-        status: 'success', 
-        data: null
-    })
-}
-
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
+//param middleware
+//apply to all that have id
+router.param('id',tourController.checkID)
 
-router.route('/').get(tourController.getAllTours).post(tourController.createATour)
+//route
+router.route('/').get(tourController.getAllTours).post(tourController.checkBody,tourController.createATour)//checkbody only apply to post
 router.route('/:id').get(tourController.getATour).patch(tourController.updateATour).delete(tourController.deleteATour)
 
 
