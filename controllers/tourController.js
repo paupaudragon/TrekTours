6789e4e5c69090576d1f3511e45905bacff60458
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-27 18:51:31 -0600

    Added filters and sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,47 +1,71 @@
-const Tour = require('./../models/tourModel')
+const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
-  try{
-    const allTours = await Tour.find()// to read all record, not passing any param in find()
+  try {
+    //Build the query
 
+    // 1)Filtering
+    const queryObj = { ...req.query }; //create a new object
+    const excludeFields = ["page", "sort", "limit", "fields"]; //depends on developer's implementation
+    excludeFields.forEach((el) => delete queryObj[el]);
+    //console.log(queryObj)
+
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+
+    //replace gte, gt, lte, lt with $ in front
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr))
+
+    //find methos returns a query
+    let query = Tour.find(JSON.parse(queryStr)); // to read all record, not passing any param in find()
+
+    // 3) Sorting 
+
+    if(req.query.sort){
+      query = query.sort(req.query.sort)
+    }
+
+
+
+    //Execute the query
+    const tours = await query;
+    console.log(req.query); //{ duration: { gte: '5' }, difficulty: 'easy' }
+
+    // Send the data
     res.status(200).json({
       status: "success",
-      results: allTours.length,
+      results: tours.length,
       data: {
-        tours: allTours,
+        tours: tours,
       },
     });
-  }catch(err){
+  } catch (err) {
     res.status(404).json({
-      status: 'fail', 
-      message: err.message
-    })
+      status: "fail",
+      message: err.message,
+    });
   }
- 
 };
 
 exports.getATour = async (req, res) => {
-
-  try{
-    const tour = await Tour.findById(req.params.id) // Tour.findOne({_id: req.params.id})
+  try {
+    const tour = await Tour.findById(req.params.id); // Tour.findOne({_id: req.params.id})
     res.status(200).json({
-        status: "success",
-        data: {
-          tours: tour,
-        },
-      });
-
-  }catch(err){
+      status: "success",
+      data: {
+        tours: tour,
+      },
+    });
+  } catch (err) {
     res.status(404).json({
-      status: 'fail', 
-      message: err.message
-    })
+      status: "fail",
+      message: err.message,
+    });
   }
-
 };
 
 exports.createATour = async (req, res) => {
-
   try {
     const newTour = await Tour.create(req.body); //MongoDb API: will ignore the data not modeled in the tourModel
     res.status(201).json({
@@ -52,54 +76,46 @@ exports.createATour = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({
-      status:'fail', 
-      message: err.message
-    })
+      status: "fail",
+      message: err.message,
+    });
   }
-
-};  
+};
 
 exports.updateATour = async (req, res) => {
-  try{
+  try {
     //mongoose API
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true, //return the new record
-      runValidators: true
+      runValidators: true,
     });
 
     res.status(200).json({
       status: "success",
       data: {
-        tour
+        tour,
       },
     });
-
-  }catch (err) {
+  } catch (err) {
     res.status(400).json({
-      status:'fail', 
-      message: err.message
-    })
-
+      status: "fail",
+      message: err.message,
+    });
   }
-  
 };
 
 exports.deleteATour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-  try{
-    await  Tour.findByIdAndDelete(req.params.id);
-  
     res.status(204).json({
       status: "success",
       data: null,
     });
-
-  }catch(err){
-
+  } catch (err) {
     res.status(400).json({
-      status:'fail', 
-      message: err.message
-    })
-
+      status: "fail",
+      message: err.message,
+    });
   }
 };
