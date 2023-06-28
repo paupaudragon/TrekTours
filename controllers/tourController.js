e47a6cf6be5f8aa04d15cc8340ecb0d82edf38f3
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-27 22:03:08 -0600

    Added aliasing and restructured the APIFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,48 +1,78 @@
 const Tour = require("./../models/tourModel");
 
-exports.getAllTours = async (req, res) => {
-  try {
-    //console.log(req.query); //{ duration: { gte: '5' }, difficulty: 'easy' }
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverge,summary,difficulty";
+  next();
+};
 
-    //Build the query
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
 
-    // 1)Filtering
-    const queryObj = { ...req.query }; //create a new object
+  filter() {
+    const queryObj = { ...this.queryString }; //create a new object
     const excludeFields = ["page", "sort", "limit", "fields"]; //depends on developer's implementation
     excludeFields.forEach((el) => delete queryObj[el]);
-    //console.log(queryObj)
 
-    // 2) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-
-    //replace gte, gt, lte, lt with $ in front
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr))
+    this.query = this.query.find(JSON.parse(queryStr));
 
-    //find methos returns a query
-    let query = Tour.find(JSON.parse(queryStr)); // to read all record, not passing any param in find()
+    //return the object so we can chain them up
+    return this;
+  }
 
-    // 3) Sorting 
-    if(req.query.sort){
-      const sortBy = req.query.sort.split(',').join(' ')
-      //console.log(sortBy)
-      query = query.sort(sortBy)
+  sort() {
+    
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+
+      this.query = this.query.sort(sortBy);
     }
     //default sorting
-    else{
-      query = query.sort('-ratingsQuantity')
+    else {
+      this.query = this.query.sort("-ratingsQuantity");
     }
 
-    // 4) Field limiting
-    if(req.query.fields){
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields)
-    }else{
-      query = query.select('-__v')//- means excluding
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v"); //- means excluding
     }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // skip x results ; x/items per page = # page skipped
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
 
     //Execute the query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     // Send the data
     res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
+//Aliasing
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+
 //route
 router.route('/').get(tourController.getAllTours).post(tourController.createATour)//checkbody only apply to post
 router.route('/:id').get(tourController.getATour).patch(tourController.updateATour).delete(tourController.deleteATour)
+
 
 
 module.exports = router; 
