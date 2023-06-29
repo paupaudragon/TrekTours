38609682aef1d8ea8e3dc06e9a96eb302b0a6c98
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-28 22:06:57 -0600

    handles non-exist urls

--- app.js
+++ app.js
@@ -16,18 +16,25 @@ const userRouter = require('./routes/userRoutes');
 //logging 
 const morgan = require('morgan');
 
-
-//Express middleware
-app.use(express.json()); // parsing incoming json data, or undefined
-
 // Scripts
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))//tiny
 }
 
+//Express middleware
+app.use(express.json()); // parsing incoming json data, or undefined
 app.use(express.static(`${__dirname}/public`)) //using public folder for accessing the front-end
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+//all means all verbs api(get, post, ...)
+//* all urls
+app.all('*', (req,res,next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+})
 
 module.exports = app;
