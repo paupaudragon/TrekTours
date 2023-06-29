4ace1cc793c442c9e102925f1f9c90ac212f39d9
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-06-29 14:54:42 -0600

    Added database error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,47 +1,79 @@
-const sendErrorDev = (err, res)=>{
+const AppError = require('./../utils/appError');
+
+const handleValidationErrorDB = err=>{
+  const errors = Object.values(err.errors).map(el=>el.message)
+  const message = `Invalid input data. ${errors.join('. ')}`
+  const error =  new AppError(message, 400)
+  console.log(error.isOperational)
+  return error
+}
+const handleDuplicateFieldsDB = err=>{
+  const field = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)[0]
+  const message = `Duplicate field value: ${field}, Please use another value.`
+  return new AppError(message, 400)
+}
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  const error =  new AppError(message, 400);
+  console.log("here")
+  console.log(error.isOperational)
+  return error
+};
+
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message, 
-        error: err,
-        stack: err.stack
-    })
-}
+      status: err.status,
+      message: err.message
+    });
 
-const sendErrorProd = (err, messares) => {
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR', err);
 
-    // Operational, trusted error: send as much error message to client
-    if(err.isOperational){
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message, 
-        })
-    }
-    // Programming or other unknown error
-    else{
-        // 1) Log error
-        console.error('Error', err)
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
 
-        // 2) Send generic message to client
-        res.status(500).json({
-            status:'error',
-            message:'Something went wrong.'
-        })
-    }
-
-}
-
+};
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500 // internal server error
-    err.status = err.status || 'error'
+  console.log(process.env.NODE_ENV);
 
-    if(process.env.NODE_ENV !== 'development'){
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-        sendErrorDev(err, res);
-
-    }else if(process.env.NODE_ENV !== 'production'){
-
-        sendErrorProd(err, res);
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error ={...err}
+    if(err.name ==='CastError') {
+      error = handleCastErrorDB(err);
     }
-    
-}
+    if(err.code === 11000){
+      error = handleDuplicateFieldsDB(err)
+    }
+    if (err.name === "ValidationError"){
+      error = handleValidationErrorDB(error);
+
+    }
+
+    sendErrorProd(error, res);
+  }
+};
