b0c95876fcfbe9ff8644ba809a1bd7e74c1186c5
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-07-17 17:11:25 -0600

    setup token when client login

--- controllers/authController.js
+++ controllers/authController.js
@@ -80,6 +80,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+    //For client login 
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
