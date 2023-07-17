67cb964f921a1ed90e479c279b762253105a39b9
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-07-17 16:55:28 -0600

    login raw alert windows works

--- public/js/login.js
+++ public/js/login.js
@@ -3,25 +3,41 @@
 
 
 // export const login = async (email, password) => {
+//     console.log(email,password);
+//     //axios have good error throwing message, so use try catch 
 //   try {
 //     const res = await axios({
 //       method: "POST",
 //       url: "http://127.0.0.1:3000/api/v1/users/login",
 //       data: {
-//         email: email,
-//         password: password,
+//         email,
+//         password
 //       },
 //     });
 
-//     if(res.data.status ==='success'){
-//       showAlert('success','Logged in');
-//       window.setTimeout(()=>{
-//         location.assign('/');
-//       }, 1500)
-//     }
+//     console.log(res);
+
+//     // if(res.data.status ==='success'){
+//     //   showAlert('success','Logged in');
+//     //   window.setTimeout(()=>{
+//     //     location.assign('/');
+//     //   }, 1500)
+//     // }
 //   } catch (err) {
-//     showAlert('error',err.response.data.message);
+//     // showAlert('error',err.response.data.message);
+//     console.log(err.response.data.message);
 //   }
 // };
+
+const login = (email, password)=>{
+    alert(email, password);
+}
+
+document.querySelector('.form').addEventListener('submit', e =>{
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+})
 
 
