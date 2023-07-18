59dea063e61b89852a039d997f6edb92930f5521
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-07-18 16:26:33 -0600

    update password

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,27 +1,52 @@
-const updateData = async (name, email) => {
+//type is either 'password' or 'data'
+const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+
     const res = await axios({
-      method: "PATCH",
-      url: "http://127.0.0.1:3000/api/v1/users/updateMe",
-      data: {
-        name,
-        email,
-      },
+      method: 'PATCH',
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert("success", "Data updated successfully!");
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
-    showAlert("error", err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
 
 const userDataForm = document.querySelector('.form-user-data'); 
-
 if(userDataForm) userDataForm.addEventListener('submit', e =>{
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
-})
+    updateSettings({name, email }, 'data');
+});
+
+const userPasswordForm = document.querySelector('.form-user-password');
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    console.log(passwordCurrent); 
+    console.log(passwordConfirm); 
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
