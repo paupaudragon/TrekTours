704361d01b06f41bf63f654402f318642e856c12
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-07-19 11:03:58 -0600

    ran scripts, no change

--- data/import-dev-data.js
+++ data/import-dev-data.js
@@ -33,9 +33,9 @@ const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf8'))
 
 const importData = async()=>{
     try{
-        // await Tour.create(tours)
+        await Tour.create(tours)
         await User.create(users, {validateBeforeSave: false})
-        // await Review.create(reviews)
+        await Review.create(reviews)
         console.log('Data successfully loaded')
     }catch(err){
         console.log(err)
@@ -47,8 +47,8 @@ const importData = async()=>{
 //Delete Data
 const deleteData = async()=>{
     try{
-        // await Tour.deleteMany()
-        // await Review.deleteMany()
+        await Tour.deleteMany()
+        await Review.deleteMany()
         await User.deleteMany()
         console.log('Data successfully deleted')
     }catch(err){
