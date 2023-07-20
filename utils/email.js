518ae8e1e86c2ce59b0200f640496ff23cb4b0d5
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-07-19 23:38:39 -0600

    fix security issue

--- utils/email.js
+++ utils/email.js
@@ -33,9 +33,6 @@ module.exports = class Email {
             auth: {
                 user: process.env.EMAIL_USERNAME, 
                 pass: process.env.EMAIL_PASSWORD
-            },
-            tls:{
-                rejectUnauthorized:false
             }
     
         }) 
